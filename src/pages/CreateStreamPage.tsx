48f82ca9b9c9cacba6266ace7c2e1ed053942193
VladimirSolovyov <vovanchik3007@mail.ru>
2024-07-24 02:17:55 +0300

    Clear project

--- src/pages/CreateStreamPage.tsx
+++ src/pages/CreateStreamPage.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import StreamForm from '../components/StreamForm'
-import styles from './CreateStreamPage.module.css'
 
 const CreateStreamPage: React.FC = () => {
 	return (
-		<div className={styles.container}>
+		<div className='createStream'>
 			<h1>Добавление Stream</h1>
 			<StreamForm />
 		</div>
